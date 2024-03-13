0b2e3b16437048d9a5995939b0faf4fe9507c859
Krishna311203 <krishnashrivastava84@gmail.com>
2024-03-13 18:00:32 +0530

    added the animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Header from "../components/header";
+
 export default function Home() {
   return <main className="">
-    hehe
-    <Header/>
+
+
   </main>;
 }
--- components/header.tsx
+++ components/header.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
+'use client';
+import React from "react";
+
+import { motion } from "framer-motion";
 
 export default function Header() {
   return (
-    <header className='z-[999] relative'>
-    <div className='fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-red  blur-[10rem]
-    sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full
-    '>
-         
-    </div>
+    <header className="z-[999] relative ">
+      <motion.div className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full "
+      initial= {{y:-100,x:"-50%", opacity:0}}
+      animate={{ y: 0,x:"-50%", opacity:1}}
 
-    <div className='bg-[#f8f8f8] absolute top-[-6rem] -z-10 right-[11rem] h-[2.25rem] w-[3.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#f9f9f9]'>
+      >
 
-    </div>  
+      </motion.div>
+
+      <nav className="fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm-py-0">
+
+      </nav>
     </header>
-  )
+  );
 }
