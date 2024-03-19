d6d570d8521d33548b768dc5ef4093502e42dd9c
Krishna311203 <krishnashrivastava84@gmail.com>
2024-03-19 20:15:06 +0530

    added header section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import Intro from "@/components/intro";
 import React from "react";
 
 export default function Home() {
   return <main className="">
 
-
+  <Intro/>
   </main>;
 }
---
+++ components/intro.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Intro() {
+  return (
+    <section>
+      <div className="flex items-center justify-center">
+        <div className="">
+          <Image src={""} alt="krishna" height='192' width='192' quality='95' priority={true} />
+        </div>
+      </div>
+
+      <p>
+      <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
+        <span className="font-bold">full-stack developer</span> with{" "}
+        <span className="font-bold">8 years</span> of experience. I enjoy
+        building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">React (Next.js)</span>.
+      </p>
+    </section>
+  );
+}
