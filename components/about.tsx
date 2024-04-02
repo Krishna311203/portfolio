f91556dd052c2830118827594a5fc6683de2a433
Krishna311203 <krishnashrivastava84@gmail.com>
2024-04-02 15:37:50 +0530

    added skills timeline and contact sections

--- components/about.tsx
+++ components/about.tsx
@@ -1,19 +1,29 @@
-'use client';
+"use client";
 import React from "react";
 import SectionHeading from "./section-heading";
-import {motion} from "framer-motion"
-export default function About() {
-  return <motion.section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
-  initial={{ opacity: 0, y: 100 }}
-  animate={{ opacity: 1, y: 0 }}
-  transition={{
-    delay: 0.175,
-  }}
-  id="about"
-  >
- <SectionHeading>About Me</SectionHeading>
+import { motion } from "framer-motion";
 
-    <p className="mb-3" >After graduating with a degree in{" "}
+import { useSectionInView } from "@/lib/hooks";
+export default function About() {
+
+  const { ref } = useSectionInView("About");
+
+
+  return (
+    <motion.section
+    ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 0.175,
+      }}
+      id="about"
+    >
+      <SectionHeading>About Me</SectionHeading>
+
+      <p className="mb-3">
+        After graduating with a degree in{" "}
         <span className="font-medium">Accounting</span>, I decided to pursue my
         passion for programming. I enrolled in a coding bootcamp and learned{" "}
         <span className="font-medium">full-stack web development</span>.{" "}
@@ -27,12 +37,17 @@ export default function About() {
         . I am also familiar with TypeScript and Prisma. I am always looking to
         learn new technologies. I am currently looking for a{" "}
         <span className="font-medium">full-time position</span> as a software
-        developer.</p>
-    <p> <span className="italic">When I'm not coding</span>, I enjoy playing
+        developer.
+      </p>
+      <p>
+        {" "}
+        <span className="italic">When I'm not coding</span>, I enjoy playing
         video games, watching movies, and playing with my dog. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
         learning about{" "}
         <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.</p>
-  </motion.section>;
+        learning how to play the guitar.
+      </p>
+    </motion.section>
+  );
 }
--- components/intro.tsx
+++ components/intro.tsx
@@ -1,13 +1,22 @@
 "use client";
-import Image from "next/image";
+
 import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { BsArrowRight, BsGithub, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
+import { useSectionInView } from "@/lib/hooks";
+import { useActiveSectionContext } from "@/context/active-section-context";
 export default function Intro() {
+  const { ref } = useSectionInView("Home", 0.5);
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+
   return (
-    <section id="home" className="mb-28 max-w-[50rem] text-center sm:m-0 scroll-mt-[100rem]">
+    <section
+      ref={ref}
+      id="home"
+      className="mb-28 max-w-[50rem] text-center sm:m-0 scroll-mt-[100rem]"
+    >
       {/* <div className="flex items-center justify-center">
         <div className="">
           <Image src={""} alt="krishna" height='192' width='192' quality='95' priority={true} />
@@ -21,11 +30,10 @@ export default function Intro() {
       >
         <span className="font-bold">Hello, I'm Krishna.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> and I enjoy{" "}
-        building <span className="italic">sites & apps</span>.{" "}I am a{" "}
+        building <span className="italic">sites & apps</span>. I am a{" "}
         <span className="font-bold">3rd Year CSE</span> Student & My focus is{" "}
         <span className="underline">Flutter, React (Next.js)</span>.
       </motion.h1>
-
 
       <motion.div
         className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium "
@@ -35,34 +43,42 @@ export default function Intro() {
           delay: 0.1,
         }}
       >
-
         <Link
           href="#contact"
           className=" group bg-gray-900 text-white px-7 py-3 flex items-center justify-center gap-2  rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition "
+          onClick={() => {
+            setActiveSection("Contact");
+            setTimeOfLastClick(Date.now());
+          }}
         >
           Contact me here
           <BsArrowRight className="opacity-70 group-hover:translate-x-2 transition" />
         </Link>
 
-        <a className="group bg-white text-black px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer border border-black/10 "  href="/CV.pdf" download>
+        <a
+          className="group bg-white text-black px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer border border-black/10 "
+          href="/CV.pdf"
+          download
+        >
           Download CV{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
 
-        <a className="bg-white text-black p-4 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer border border-black/10 "
-        href="https://www.linkedin.com/in/krishna-shrivastava-/"
-        target="_blank"
+        <a
+          className="bg-white text-black p-4 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer border border-black/10 "
+          href="https://www.linkedin.com/in/krishna-shrivastava-/"
+          target="_blank"
         >
           <BsLinkedin />
         </a>
 
-        <a className="bg-white text-black p-4 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer border border-black/10 "
-        href="https://github.com/Krishna311203"
-        target="_blank"
+        <a
+          className="bg-white text-black p-4 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer border border-black/10 "
+          href="https://github.com/Krishna311203"
+          target="_blank"
         >
           <BsGithub />
         </a>
-
       </motion.div>
     </section>
   );
