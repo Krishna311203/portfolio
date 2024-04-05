55443aa3e3e80dc59117740273250e60011fd11f
Krishna311203 <krishnashrivastava84@gmail.com>
2024-04-05 22:00:15 +0530

    revert to experience section

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import About from "@/components/about";
 import Projects from "@/components/projects";
 import Skills from "@/components/skills";
 import Experience from "@/components/experience";
-import Contact from "@/components/contact";
+
 
 export default function Home() {
   return <main className="flex flex-col items-center justify-center px-4 ">
@@ -16,6 +16,5 @@ export default function Home() {
   <Projects/>
   <Skills/>
   <Experience/>
-  <Contact/>
   </main>;
 }
