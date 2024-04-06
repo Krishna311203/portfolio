759011b55da3300e4d8eee88c68ea16e4082aa34
Krishna311203 <krishnashrivastava84@gmail.com>
2024-04-06 22:58:40 +0530

    added dark mode features and footer

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import About from "@/components/about";
 import Projects from "@/components/projects";
 import Skills from "@/components/skills";
 import Experience from "@/components/experience";
+import Footer from "@/components/footer";
 
 
 export default function Home() {
@@ -16,5 +17,6 @@ export default function Home() {
   <Projects/>
   <Skills/>
   <Experience/>
+  <Footer/>
   </main>;
 }
--- components/about.tsx
+++ components/about.tsx
@@ -23,30 +23,24 @@ export default function About() {
       <SectionHeading>About Me</SectionHeading>
 
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
+        I am currently pursuing a degree in{" "}
+        <span className="font-medium">Computer Science Engineering with specialization in AI and ML</span>from SRM Institute of Science and Technology.I am urrently Working on{" "}
+        <span className="font-medium">full-stack web and flutter development</span>.{" "}
         <span className="italic">My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
         is{" "}
         <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
+          React, Next.js, Flutter, Node.js and MongoDB
         </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
+        . I am also familiar with DevOps concepts. I am always looking to
         learn new technologies. I am currently looking for a{" "}
         <span className="font-medium">full-time position</span> as a software
         developer.
       </p>
       <p>
         {" "}
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        <span className="italic">When I'm not coding</span>, I enjoy playing basketball, reading books and watching movies.
       </p>
     </motion.section>
   );
